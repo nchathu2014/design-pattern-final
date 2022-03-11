97d964fd6321af1398c35054b8a3ea6939f48d1b
Chathuranga <nchathu2014@gmail.com>
2022-03-11 10:53:41 +0530

    Proxy pattern complete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Design Pattern Final
-Created with CodeSandbox
+# Proxy Design Pattern
+
+With a Proxy we can get more control over the interactions with certain object. We are not directly communicate with the target object, instead Proxy will do. Building "Validation Libraries" and "Caching Machanisms", we can use Proxys.
+
+![alt text](https://github.com/nchathu2014/design-pattern-final/blob/pattern/proxy/src/images/proxy_pattern.JPG?raw=true)
--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import "./styles.css";
+import image from "./images/proxy_pattern.JPG";
+import coin from "./utils/proxy";
 
 export default function App() {
+  const getBitCoin = () => {
+    console.log(coin.getCoinData("Bitcoin"));
+  };
+
+  const getLiteCoin = () => {
+    console.log(coin.getCoinData("Litecoin"));
+  };
+
+  const getDogCoin = () => {
+    console.log(coin.getCoinData("Dogcoin"));
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Proxy Design Pattern</h1>
+      <h2>
+        With a Proxy we can get more control over the interactions with certain
+        object. We are not directly communicate with the target object, instead
+        Proxy will do. Building "Validation Libraries" and "Caching Machanisms",
+        we can use Proxys.
+      </h2>
+      <img src={image} width={1200} height={300} />
+      <hr />
+
+      <h3>
+        Only calling to the API at first fetch, afterwords getting data from the
+        cache (proxy)
+      </h3>
+      <button onClick={getBitCoin}>Bitcoin</button>
+      <button onClick={getLiteCoin}>Litecoin</button>
+      <button onClick={getDogCoin}>Dogcoin</button>
     </div>
   );
 }
