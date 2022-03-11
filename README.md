99a2d08fd27cc24d5291a4bfb0eb20a712beb2b3
Chathuranga <nchathu2014@gmail.com>
2022-03-11 21:47:10 +0530

    Add image

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Created with CodeSandbox
 
-![alt text](https://github.com/nchathu2014/design-pattern-final/blob/pattern/mediator/src/images/mediator_pattern.JPG?raw=true)
+![alt text](https://github.com/nchathu2014/design-pattern-final/blob/master/src/images/test.JPG?raw=true)
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
-
+import image from "./images/test.JPG";
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <img src={image} width={500} height={500} />
     </div>
   );
 }
