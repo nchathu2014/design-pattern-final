36f5c7ad153afdc1f0573f430bfd6cad00b0195c
Chathuranga <nchathu2014@gmail.com>
2022-03-15 00:28:40 +0530

    Updated all project files

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
-# Container/ Presentational Design Pattern
+# Design Pattern Final
 
-Enforce separation of concerns by separating the view from the application logic.
+Created with CodeSandbox
 
-In React, one way to enforce separation of concerns is by using
-the Container/Presentational pattern. With this pattern, we can separate the
-view from the application logic.
-Let's say we want to create an application that fetches 6 dog images, and
-renders these images on the screen. Ideally, we want to enforce separation of
-concerns by separating this process into two parts:
-
-1. Presentational Components: Components that care about how data is
-   shown to the user. In this example, that's the rendering the list of dog
-   images.
-2. Container Components: Components that care about what data is shown
-   to the user.
-
-![alt text](https://github.com/nchathu2014/design-pattern-final/blob/master/src/images/test.JPG?raw=true)
+![alt text](https://github.com/nchathu2014/design-pattern-final/blob/master/src/images/test.jpg?raw=true)
--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import "./styles.css";
-import image from "./images/container_presentational_pattern.JPG";
-import Container from "./components/Container";
+import image from "./images/test.jpg";
 export default function App() {
   return (
     <div className="App">
-      <h1>Container/ Presentational Design Pattern</h1>
-      <Container />
-      <h2>
-        Enforce separation of concerns by separating the view from the
-        application logic. In React, one way to enforce separation of concerns
-        is by using the Container/Presentational pattern. With this pattern, we
-        can separate the view from the application logic. Let's say we want to
-        create an application that fetches 6 dog images, and renders these
-        images on the screen. Ideally, we want to enforce separation of concerns
-        by separating this process into two parts:
-        <ol style={{ textAlign: "left" }}>
-          <li>
-            <b>Presentational Components:</b>
-            <p>
-              Components that care about how data is shown to the user. In this
-              example, that's the rendering the list of dog images.
-            </p>
-          </li>
-          <li>
-            <b>Container Components:</b>
-            <p>Components that care about what data is shown to the user.</p>
-          </li>
-        </ol>
-      </h2>
-      <img src={image} width={400} height={400} />
-
-      <hr />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <img src={image} width={500} height={500} />
     </div>
   );
 }
