0eb4d18c4d2e1ef9e20fe404877c357de1393d15
Chathuranga <nchathu2014@gmail.com>
2022-03-15 07:16:29 +0530

    Module pattern started

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Design Pattern Final
+# Module Design Pattern
 
-Created with CodeSandbox
+Split up your code into smaller, reusable pieces.
+
+As your application and codebase grow, it becomes increasingly important to
+keep your code maintainable and separated. The module pattern allows you
+to split up your code into smaller, reusable pieces.
 
 ![alt text](https://github.com/nchathu2014/design-pattern-final/blob/master/src/images/test.jpg?raw=true)
--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import image from "./images/test.jpg";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Module Design Pattern</h1>
+      <h2>
+        <p>Split up your code into smaller, reusable pieces.</p>
+
+        <p>
+          As your application and codebase grow, it becomes increasingly
+          important to keep your code maintainable and separated. The module
+          pattern allows you to split up your code into smaller, reusable
+          pieces.
+        </p>
+      </h2>
       <img src={image} width={500} height={500} />
     </div>
   );
